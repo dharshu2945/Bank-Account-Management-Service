e4ab5eb77bf179d3b49cc319e76e66852f4fa549
dharshu2945 <dharshini2945@gmail.com>
2026-01-22 08:55:50 +0530

    Refactor InvalidCustomerException class
    Removed serialVersionUID and constructor from InvalidCustomerException.

--- src/com/wipro/bams/util/InvalidCustomerException.java
+++ src/com/wipro/bams/util/InvalidCustomerException.java
@@ -1,11 +1,6 @@
 package com.wipro.bams.util;
 
 public class InvalidCustomerException extends Exception{
-	private static final long serialVersionUID = 1L; 
-	public InvalidCustomerException(String message) {
-		super(message);
-	}
-
 	@Override
 	public String toString() {
 		return "InvalidCustomerException"+getMessage();
