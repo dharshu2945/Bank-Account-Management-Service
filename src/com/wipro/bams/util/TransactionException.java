d3a0a1ed027a6341e3cccee3965199362dff350f
dharshu2945 <dharshini2945@gmail.com>
2026-01-22 08:59:01 +0530

    Refactor TransactionException class
    Removed constructor and serialVersionUID from TransactionException.

--- src/com/wipro/bams/util/TransactionException.java
+++ src/com/wipro/bams/util/TransactionException.java
@@ -1,11 +1,6 @@
 package com.wipro.bams.util;
 
 public class TransactionException extends Exception{
-	private static final long serialVersionUID = 1L; 
-	public TransactionException(String message) {
-		super(message);
-	}
-
 	@Override
 	public String toString() {
 		return "TransactionException"+getMessage();
