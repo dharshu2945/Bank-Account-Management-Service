8f14ed7b2aab4d88b96415de4e8c5e94cd8a8855
dharshu2945 <dharshini2945@gmail.com>
2026-01-22 08:54:10 +0530

    Refactor InsufficientBalanceException class
    Removed constructor and serialVersionUID from InsufficientBalanceException.

--- src/com/wipro/bams/util/InsufficientBalanceException.java
+++ src/com/wipro/bams/util/InsufficientBalanceException.java
@@ -1,12 +1,6 @@
 package com.wipro.bams.util;
 
 public class InsufficientBalanceException extends Exception{
-	private static final long serialVersionUID = 1L; 
-
-	public InsufficientBalanceException(String message) {
-		super(message);
-	}
-
 	@Override
 	public String toString() {
 		return "InsufficientBalanceException"+getMessage();
